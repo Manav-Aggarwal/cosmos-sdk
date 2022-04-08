bdcd75b4523d28ab7cf2c2ac5fb0a18d94421f06
Manav Aggarwal <manavaggarwal1234@gmail.com>
2022-04-08 12:58:08 +0200

    Add ISRs to BeginBlock and EndBlock

--- store/rootmulti/store.go
+++ store/rootmulti/store.go
@@ -365,6 +365,28 @@ func (rs *Store) LastCommitID() types.CommitID {
 	return rs.lastCommitInfo.CommitID()
 }
 
+func (rs *Store) IntermediateStateRoot() (rootHash []byte, err error) {
+	var previousHeight, version int64
+	if rs.lastCommitInfo.GetVersion() == 0 && rs.initialVersion > 1 {
+		// This case means that no commit has been made in the store, we
+		// start from initialVersion.
+		version = rs.initialVersion
+
+	} else {
+		// This case can means two things:
+		// - either there was already a previous commit in the store, in which
+		// case we increment the version from there,
+		// - or there was no previous commit, and initial version was not set,
+		// in which case we start at version 1.
+		previousHeight = rs.lastCommitInfo.GetVersion()
+		version = previousHeight + 1
+	}
+
+	rs.lastCommitInfo = commitStores(version, rs.stores)
+	hash := rs.lastCommitInfo.Hash()
+	return hash, nil
+}
+
 // Commit implements Committer/CommitStore.
 func (rs *Store) Commit() types.CommitID {
 	var previousHeight, version int64
